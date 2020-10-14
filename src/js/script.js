6e37839a4b417cd141e1e31116bca2268a79b8ad
celepci <muhammet.celepci@pixelate.com.tr>
2020-10-15 01:50:37 +0300

    Restoran sayfası düzenlendi

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
-function togglePassSee(){
+function togglePassSee() {
     var input = $('.password input');
     var type = input.attr("type");
     $('.password-see').toggleClass('active');
-    if(type == "password"){
-        input.attr("type","text");
-    }else{
-        input.attr("type","password");
+    if (type == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
     }
 }
+
+$('.tab-item').on('click', function () {
+    var target = $(this).data('href');
+    $('.tabcontent,.tab-item').removeClass('active');
+    $('.restaruant-tabs-item[data-href="'+target+'"]').addClass('active');
+    $('#'+target).addClass('active');
+});
